af822650edacf132e7c55eb5eeebcf9e71754298
Jón Böðvarsson <jonbodvar93@gmail.com>
2021-12-03 23:33:27 +0000

    Add better solution for Day3, part 1

--- 2021/3/part1.js
+++ 2021/3/part1.js
@@ -1,9 +1,9 @@
-const { processInput } = require("./input");
+const { getInputLines, getBitCount } = require("./shared");
 
-const generateBinaryString = (processedInput) => {
+const generateBinaryString = (bitCount, numLines) => {
     var result = "";
-    for (const prop in processedInput.bitCount) {
-        if (processedInput.bitCount[prop] < processedInput.numLines / 2) {
+    for (const prop in bitCount) {
+        if (bitCount[prop] < numLines / 2) {
             result += "0";
         } else {
             result += "1";
@@ -12,12 +12,19 @@ const generateBinaryString = (processedInput) => {
     return result;
 }
 
-const calculate = (processedInput) => {
-    const gammaBinary = generateBinaryString(processedInput);
-    const epsilonBinary = gammaBinary.split("").map(char => char == "0" ? "1" : "0").join("");
+const flipAllBits = (binary) => {
+    return binary.split("").map(char => char == "0" ? "1" : "0").join("");
+}
+
+const calculate = (bitCount, numLines) => {
+    const gammaBinary = generateBinaryString(bitCount, numLines);
+    const epsilonBinary = flipAllBits(gammaBinary);
     const gamma = parseInt(gammaBinary, 2);
     const epsilon = parseInt(epsilonBinary, 2);
     console.log("Solution to 3.1", gamma * epsilon);
 };
 
-processInput(calculate);
+const lines = getInputLines();
+const bitCount = getBitCount(lines);
+
+calculate(bitCount, lines.length);
