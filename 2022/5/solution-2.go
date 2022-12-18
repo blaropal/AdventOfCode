481fe6b57d4a11a8d072a0bf72023bc72d88b05d
Jon Bodvarsson <jonbo@N-LAP0015.local>
2022-12-18 22:05:06 +0000

    Day 5 - 2022

---
+++ 2022/5/solution-2.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type RuneSlice []rune
+
+func (r *RuneSlice) prepend(val rune) {
+	*r = append(*r, 0)
+	copy((*r)[1:], *r)
+	(*r)[0] = val
+}
+
+func (r *RuneSlice) pop() rune {
+	ret := (*r)[0]
+	*r = (*r)[1:]
+	return ret
+}
+
+func (r *RuneSlice) insertAt(index int, val rune) {
+	if index == 0 {
+		r.prepend(val)
+		return
+	}
+
+	if index >= len(*r) {
+		*r = append(*r, val)
+		return
+	}
+	*r = append((*r)[:index+1], (*r)[index:]...)
+	(*r)[index] = val
+}
+
+func main() {
+	readFile, err := os.Open("./input.txt")
+	defer readFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	slices := map[int]RuneSlice{}
+
+	// Populate slices
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		if len(line) == 0 {
+			break
+		}
+
+		// Run through all columns and check for values
+		for i := 0; i*4 < len(line); i++ {
+			r := rune(line[(i*4)+1])
+			if unicode.IsLetter(r) {
+				slices[i] = append(slices[i], r)
+			}
+		}
+
+	}
+
+	// Perform movements
+	for fileScanner.Scan() {
+		parts := strings.Split(fileScanner.Text(), " ")
+		moveCount, _ := strconv.Atoi(parts[1])
+		fromCol, _ := strconv.Atoi(parts[3])
+		toCol, _ := strconv.Atoi(parts[5])
+
+		fromSlice := slices[fromCol-1]
+		toSlice := slices[toCol-1]
+		for i := 0; i < moveCount; i++ {
+			toSlice.insertAt(i, fromSlice.pop())
+			slices[toCol-1] = toSlice
+			slices[fromCol-1] = fromSlice
+		}
+	}
+
+	sol := ""
+	for i := 0; i < len(slices); i++ {
+		if len(slices[i]) > 0 {
+			sol += string(slices[i][0])
+		}
+	}
+
+	fmt.Printf("Solution to 5.2: %s\n", sol)
+}
